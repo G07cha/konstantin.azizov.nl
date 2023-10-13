85f229148e8176a7df456cc67f17a1d3a0f872a3
Konstantin Azizov <azizovkostya97@gmail.com>
2023-10-13 15:25:14 +0200

    chore: add code formatting

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,22 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import { createHtmlPlugin } from 'vite-plugin-html'
-import { viteSingleFile } from "vite-plugin-singlefile"
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
   plugins: [
     createHtmlPlugin({ minify: true }),
-    viteSingleFile({ inlinePattern: ['**/*.css'], useRecommendedBuildConfig: false })
+    viteSingleFile({
+      inlinePattern: ["**/*.css"],
+      useRecommendedBuildConfig: false,
+    }),
   ],
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        blog: resolve(__dirname, 'blog/index.html'),
+        main: resolve(__dirname, "index.html"),
+        blog: resolve(__dirname, "blog/index.html"),
       },
     },
-  }
-})
+  },
+});
